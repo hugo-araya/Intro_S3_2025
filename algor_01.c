be33c2547afe7f6d3080e50548a021e9edc54103
Hugo Araya C. <42989121+hugo-araya@users.noreply.github.com>
2025-05-16 11:34:46 -0400

    segundo

---
+++ algor_01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, suma = 0;
+    printf("Ingrese el primer numero\n");
+    scanf("%d",&a);
+    printf("Ingrese el segundo numero\n");
+    scanf("%d",&b);
+    suma = a + b;
+    printf("resultado:%d\n\a", suma);
+    return 0;
+}
